622adddb83530481243dfe6dcf94ddee66054f34
Egil Johnsen Dahle <egil_dahle@hotmail.com>
2026-01-13 10:48:47 +0100

    Work on footer

--- src/frontend/header-footer.ts
+++ src/frontend/header-footer.ts
@@ -1,9 +1,15 @@
-
 (() => {
     const body = document.body;
     if (!body) return;
     const header = document.createElement("header");
     const footer = document.createElement("footer");
+    header.innerHTML = "<h1> YO dude</h1>";
     body.prepend(header);
     body.appendChild(footer);
+
+    const footerIMG = document.createElement("img");
+    footerIMG.src = "./img/footer-img.svg";
+    const lederText = document.createElement("p");
+    lederText.innerText = "Ansvarlig leder: Thomas \"The GOAT\" Johansen!";
+    footer.append(footerIMG, lederText);
 })();
