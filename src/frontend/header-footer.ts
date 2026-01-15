2499906ca5a067ac3e9b1450c6707da3b9e9f1f7
Egil Johnsen Dahle <egil_dahle@hotmail.com>
2026-01-15 12:57:36 +0100

    Add structured footer layout and content
    Introduces a grid-based footer layout in CSS and updates header-footer.ts to create a structured footer with editor info, contact link, image, and additional resource links. Enhances page organization and accessibility of footer information.

--- src/frontend/header-footer.ts
+++ src/frontend/header-footer.ts
@@ -7,9 +7,27 @@
     body.prepend(header);
     body.appendChild(footer);
 
+    //Creating the FOOTER!👍
+    const redaktør = document.createElement("div");
+    const bilde = document.createElement("div");
+    const om = document.createElement("div");
+    footer.append(redaktør, bilde, om);
+
     const footerIMG = document.createElement("img");
     footerIMG.src = "./img/footer-img.svg";
+    bilde.append(footerIMG);
+
     const lederText = document.createElement("p");
     lederText.innerText = "Ansvarlig leder: Thomas \"The GOAT\" Johansen!";
-    footer.append(footerIMG, lederText);
+    const epostText = document.createElement("a");
+    epostText.innerText = "Kontaktinformasjon: emgl@hopvgskole.no";
+    epostText.href = "mailto:emgl@hopvgskole.no";
+    redaktør.append(lederText, epostText);
+
+    const omOss = document.createElement("a");
+    omOss.innerText = "Se \"Om oss\" for mer informasjon.";
+    const skolen = document.createElement("a");
+    skolen.innerText = "Finn ut mer om skolen vår!";
+    om.append(omOss, skolen);
 })();
+
